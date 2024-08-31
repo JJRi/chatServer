27cb9ed3656422310e3ffe6464c9050d5ced7f0f
jari <jari.rinnemaki@gmail.com>
2024-08-31 10:52:18 +0300

    from eclipse to src

---
+++ ChatServer.java
@@ -0,0 +1,27 @@
+
+//https://hackr.io/blog/how-to-build-a-java-chat-app
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ChatServer {
+	
+	private static List<ClientHandler> clients = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException {
+		
+		ServerSocket serverSocket = new ServerSocket(15000);
+		System.out.println("Server started. Waiting for connections.");
+		
+		while (true) {
+			Socket clientSocket = serverSocket.accept();
+			System.out.println("Client connected." + clientSocket);
+			
+			ClientHandler clientThread = new ClientHandler(clientSocket, clients);
+			clients.add(clientThread);
+			new Thread(clientThread).start();
+		}
+		
+		
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Java practice work
+
+This is the serveside of the chat application
+
+Template used:
+https://hackr.io/blog/how-to-build-a-java-chat-app
